3ba8d87d6e8a23160777f61ae9dfbc6b3b58d889
pbsvk <152455231+pbsvk@users.noreply.github.com>
2024-01-03 13:55:01 -0500

    Initial Commit

---
+++ Messenger/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Messenger
+//
+//  Created by Bhaskara Sai Vamsi Krishna Padala on 1/3/24.
+//
+//Getting Stared
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
