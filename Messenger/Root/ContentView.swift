f27e0b240a503454d8d85c2a72e7817960d99de5
pbsvk <152455231+pbsvk@users.noreply.github.com>
2024-01-04 15:20:36 -0500

    Day-1

--- Messenger/Root/ContentView.swift
+++ Messenger/Root/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Let's Get started!!")
+            Text("Messenger Loading")
         }
         .padding()
     }
